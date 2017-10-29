ee7ce0f1235d647a5a8bb3c6b23dbb2854b8a8af
apibrac <alexis.pibrac@gmail.com>
2017-10-29 23:21:44 +0100

    Object orriented programation

--- 04_nmt_generator/build_nmt.js
+++ 04_nmt_generator/build_nmt.js
@@ -1,39 +1,49 @@
 var { newFile } = require('./xml_builder.js');
 
-var file = newFile('loyal.nmt');
+function nmtFile(xmlFile){
+  return new nmtNode(
+    file.content
+      .node('animata').attr({version: '0.004'})
+  )
+}
 
-var root = file.content
-  .node('animata').attr({version: '0.004'});
+function nmtNode(xmlNode){
+  this.node = xmlNode;
+}
 
-addLayer(root, {file: "loyal.png"});
-
-file.save();
-
-function addLayer(node, {file, name = file.slice(0, -4)}){
-  addRectangle(
-    node
-      .node('layer').attr({name, x: 0, y: 0, z:0})
+nmtNode.prototype = {
+  addLayer({file, name = file.slice(0, -4)}){
+    var mesh = this.node
+      .node('layer').attr({name, x: 0, y: 0, z: 0})
         .node('texture').attr({location: file, x: 0, y: 0, scale: 0.2})
       .parent()
-        .node('mesh'),
-    {x1: 1942, y1: 2046}
-  );
+        .node('mesh')
+    new nmtNode(mesh).addRectangle({x1: 1942, y1: 2046});
+  },
+
+  addRectangle({x0 = 0, y0 = 0, x1, y1}){
+    this
+      .addVertex({x: x0, y: y0, u: 0, v: 0})
+      .addVertex({x: x0, y: y1, u: 0, v: 5})
+      .addVertex({x: x1, y: y1, u: 5, v: 5})
+      .addVertex({x: x1, y: y0, u: 5, v: 0})
+    this.node
+      .node('faces')
+        .node('face').attr({v0: 0, v1: 3, v2: 2})
+      .parent()
+        .node('face').attr({v0: 0, v1: 1, v2: 2});
+  },
+
+  addVertex(opts){
+    this.node
+      .node('vertex').attr({selected: 0, ...opts});
+    return this;
+  }
 }
 
-function addRectangle(node, {x0 = 0, y0 = 0, x1, y1}){
-  node
-    .node('vertices')
-      .node('vertex').attr({x: x0, y: y0, u: 0, v: 0, selected: 0})
-    .parent()
-      .node('vertex').attr({x: x0, y: y1, u: 0, v: 5, selected: 0})
-    .parent()
-      .node('vertex').attr({x: x1, y: y1, u: 5, v: 5, selected: 0})
-    .parent()
-      .node('vertex').attr({x: x1, y: y0, u: 5, v: 0, selected: 0})
-    .parent()
-  .parent()
-    .node('faces')
-      .node('face').attr({v0: 0, v1: 3, v2: 2})
-    .parent()
-      .node('face').attr({v0: 0, v1: 1, v2: 2});
-}
+var file = newFile('loyal.nmt');
+
+nmtFile(file)
+  .addLayer({file: "loyal.png"})
+
+file.save();
