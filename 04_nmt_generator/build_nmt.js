17b1ab71206ce30253d46e8c0f29ddb3beca5129
apibrac <alexis.pibrac@gmail.com>
2017-11-08 20:44:01 +0100

    Extend node prototype for nmt design

--- 04_nmt_generator/build_nmt.js
+++ 04_nmt_generator/build_nmt.js
@@ -1,45 +1,49 @@
 var { newFile } = require('./xml_builder.js');
+var libxml = require("libxmljs");
 
 function nmtFile(xmlFile){
-  return new nmtNode(
-    file.content
-      .node('animata').attr({version: '0.004'})
-  )
+  return xmlFile.content
+    .node('animata').attr({version: '0.004'});
 }
 
-function nmtNode(xmlNode){
-  this.node = xmlNode;
-}
+let node = new libxml.Document();
+node = node.node('test');
 
-nmtNode.prototype = {
+Object.assign(Object.getPrototypeOf(node), {
   addLayer({file, name = file.slice(0, -4)}){
-    var mesh = this.node
+    return this
       .node('layer').attr({name, x: 0, y: 0, z: 0})
         .node('texture').attr({location: file, x: 0, y: 0, scale: 0.2})
       .parent()
         .node('mesh')
-    new nmtNode(mesh).addRectangle({x1: 1942, y1: 2046});
+          .addRectangle({x1: 1942, y1: 2046})
+        .parent()
+      .parent()
+    .parent();
   },
 
   addRectangle({x0 = 0, y0 = 0, x1, y1}){
-    this
-      .addVertex({x: x0, y: y0, u: 0, v: 0})
-      .addVertex({x: x0, y: y1, u: 0, v: 5})
-      .addVertex({x: x1, y: y1, u: 5, v: 5})
-      .addVertex({x: x1, y: y0, u: 5, v: 0})
-    this.node
-      .node('faces')
-        .node('face').attr({v0: 0, v1: 3, v2: 2})
+    return this
+      .node('vertices')
+        .addVertex({x: x0, y: y0, u: 0, v: 0})
+        .addVertex({x: x0, y: y1, u: 0, v: 5})
+        .addVertex({x: x1, y: y1, u: 5, v: 5})
+        .addVertex({x: x1, y: y0, u: 5, v: 0})
       .parent()
-        .node('face').attr({v0: 0, v1: 1, v2: 2});
+        .node('faces')
+          .node('face').attr({v0: 0, v1: 3, v2: 2})
+        .parent()
+          .node('face').attr({v0: 0, v1: 1, v2: 2})
+        .parent()
+      .parent();
   },
 
   addVertex(opts){
-    this.node
-      .node('vertex').attr({selected: 0, ...opts});
-    return this;
+    return this
+      .node('vertex').attr({selected: 0, ...opts})
+    .parent();
   }
-}
+})
 
 var file = newFile('loyal.nmt');
 
