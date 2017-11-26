73ee9f66d242de86b804211dd3d6c00bb53908fd
apibrac <alexis.pibrac@gmail.com>
2017-11-26 21:50:38 +0100

    First image

--- 11_react_native_app/lumyon-app/components/Creator.js
+++ 11_react_native_app/lumyon-app/components/Creator.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default () =>
   <View>
     <Text>Lets create</Text>
+    <Image source={require('../img/ligue_des_champions.jpg')}/>
   </View>
 
