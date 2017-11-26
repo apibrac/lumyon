a1b9fc03f20558224a42e15b7cb33aff740a0daa
apibrac <alexis.pibrac@gmail.com>
2017-11-26 21:43:21 +0100

    Separate file for separate components

--- 11_react_native_app/lumyon-app/App.js
+++ 11_react_native_app/lumyon-app/App.js
@@ -1,48 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NativeRouter, Route, Link } from 'react-router-native';
+import Creator from './components/Creator';
 
-const SERVER_ENDPOINT = "http://192.168.0.12:3000/";
-
-const Creation = () => <Text>Création</Text>
-
-export default class App extends React.Component {
-  state = {
-    result: null,
-  };
-
-  componentDidMount() {
-    this._fetchExampleAsync();
-  }
-
-  _fetchExampleAsync = async () => {
-    try {
-      let response = await fetch(SERVER_ENDPOINT);
-      let result = await response.json();
-      this.setState({result});
-    } catch(e) {
-      this.setState({result: e});
-    }
-  };
-
-  render() {
-    return (
-      <NativeRouter>
-        <View style={styles.container}>
-          <Route exact path="/" render={() => (
-            <View>
-              <Text>Welcome</Text>
-              <Link to="/creation">
-                <Text>Create</Text>
-              </Link>
-            </View>
-          )} />
-          <Route path="/creation" component={Creation}/>
+export default () =>
+  <NativeRouter>
+    <View style={styles.container}>
+      <Route exact path="/" render={() => (
+        <View>
+          <Text>Welcome</Text>
+          <Link to="/creation">
+            <Text>Create</Text>
+          </Link>
         </View>
-      </NativeRouter>
-    );
-  }
-}
+      )} />
+      <Route path="/creation" component={Creator}/>
+    </View>
+  </NativeRouter>
 
 const styles = StyleSheet.create({
   container: {
---
+++ 11_react_native_app/lumyon-app/components/Creator.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default () =>
+  <View>
+    <Text>Lets create</Text>
+  </View>
+
